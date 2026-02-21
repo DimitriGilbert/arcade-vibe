6e2d27b7c66caf9144ebc1f8e4274cd2387dfe82
DimitriGilbert <dimitri.gilbert@gmail.com>
2026-02-21 15:07:30 +0100

    migration fix

--- packages/db/src/migrations/0017_soft_kate_bishop.sql
+++ packages/db/src/migrations/0017_soft_kate_bishop.sql
@@ -1,1 +1,2 @@
-ALTER TABLE "model_config" ADD COLUMN "model_created_at" timestamp NOT NULL;
+TRUNCATE TABLE "model_config";
+ALTER TABLE "model_config" ADD COLUMN "model_created_at" timestamp NOT NULL DEFAULT now();
