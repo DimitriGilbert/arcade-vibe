1872b6a59bc3a3d4101df4d57a0a0b5ded3601af
DimitriGilbert <dimitri.gilbert@gmail.com>
2026-02-22 10:45:42 +0100

    massive cleanup

--- apps/web/src/components/creator/codelab/index.ts
+++
@@ -1,6 +0,0 @@
-export { CodelabSidebar } from "./codelab-sidebar";
-export { CodelabModelPicker } from "./codelab-model-picker";
-export { CodelabPromptCell } from "./codelab-prompt-cell";
-export { CodelabOutputCell } from "./codelab-output-cell";
-export { CodelabRunHistory } from "./codelab-run-history";
-export * from "./types";
